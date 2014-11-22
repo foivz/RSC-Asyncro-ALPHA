3710b2f4040594f70e350d05c9a552670d61c6d9
Denis Pavlović <dpavlovi@foi.hr>
2014-11-22 22:59:38 +0100

    Autoloaders

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit09065fb7acd23fc4b393dbf0fe721ef7::getLoader();
+return ComposerAutoloaderInitfc9c4fce6c0e6ccec65f15ef9d651d05::getLoader();
