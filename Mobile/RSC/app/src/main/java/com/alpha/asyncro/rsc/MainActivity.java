c618d6b45a80ae8ed41b4c56657a05b1b6314595
Lovro Predovan <lovro.predovan@gmail.com>
2014-11-22 22:36:20 +0100

    Added push notifications V1

--- Mobile/RSC/app/src/main/java/com/alpha/asyncro/rsc/MainActivity.java
+++ Mobile/RSC/app/src/main/java/com/alpha/asyncro/rsc/MainActivity.java
@@ -1,9 +1,15 @@
 package com.alpha.asyncro.rsc;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Typeface;
+import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.alpha.asyncro.rsc.data.controller.UserController;
@@ -15,11 +21,17 @@ import com.alpha.asyncro.rsc.fragment.InstitutionsFragment;
 import com.alpha.asyncro.rsc.fragment.UserFragment;
 import com.alpha.asyncro.rsc.util.Preferences;
 import com.astuetz.PagerSlidingTabStrip;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
 import com.lightandroid.data.api.listener.OnDataMultipleResponseListener;
 import com.lightandroid.data.api.listener.OnErrorListener;
 import com.lightandroid.navigation.activity.LightTabbedActivity;
 import com.lightandroid.type.LightData;
 import com.lightandroid.util.LightFont;
+
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import butterknife.InjectView;
 import retrofit.RetrofitError;
@@ -29,6 +41,36 @@ import retrofit.client.Response;
  * Created by dmacan on 22.11.2014..
  */
 public class MainActivity extends LightTabbedActivity implements OnDataMultipleResponseListener, OnErrorListener {
+
+
+    /**
+     * GCM
+     */
+    public static final String EXTRA_MESSAGE = "message";
+    public static final String PROPERTY_REG_ID = "registration_id";
+    private static final String PROPERTY_APP_VERSION = "appVersion";
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    /**
+     * Substitute you own sender ID here. This is the project number you got
+     * from the API Console, as described in "Getting Started."
+     */
+    String SENDER_ID = "957145599987";
+
+    /**
+     * Tag used on log messages.
+     */
+    static final String TAG = "GCMDemo";
+
+    GoogleCloudMessaging gcm;
+    AtomicInteger msgId = new AtomicInteger();
+    SharedPreferences prefs;
+    Context context;
+    String regid;
+
+
+
+
+
 
     @InjectView(R.id.pager)
     ViewPager pager;
@@ -60,6 +102,29 @@ public class MainActivity extends LightTabbedActivity implements OnDataMultipleR
             EventsFragment eventsFragment = new EventsFragment();
             eventsFragment.setLabel(getString(R.string.mic_event));
             fragments = new Fragment[]{userFragment, donorCardFragment, institutionsFragment, eventsFragment};
+
+            /**
+             * GCM
+             */
+
+            context = getApplicationContext();
+
+            // Check device for Play Services APK. If check succeeds, proceed with
+            //  GCM registration.
+            if (checkPlayServices()) {
+                gcm = GoogleCloudMessaging.getInstance(this);
+                regid = getRegistrationId(context);
+
+                if (regid.isEmpty()) {
+                    registerInBackground();
+                }
+            } else {
+                Log.i(TAG, "No valid Google Play Services APK found.");
+            }
+
+
+
+
         }
         return fragments;
     }
@@ -105,4 +170,140 @@ public class MainActivity extends LightTabbedActivity implements OnDataMultipleR
     public UserController getUserController() {
         return userController;
     }
+
+    /**
+     * Check the device to make sure it has the Google Play Services APK. If
+     * it doesn't, display a dialog that allows users to download the APK from
+     * the Google Play Store or enable it in the device's system settings.
+     */
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Log.i(TAG, "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+
+    /**
+     * Gets the current registration ID for application on GCM service.
+     * <p>
+     * If result is empty, the app needs to register.
+     *
+     * @return registration ID, or empty string if there is no existing
+     *         registration ID.
+     */
+    private String getRegistrationId(Context context) {
+        final SharedPreferences prefs = getGCMPreferences(context);
+        String registrationId = prefs.getString(PROPERTY_REG_ID, "");
+        if (registrationId.isEmpty()) {
+            Log.i(TAG, "Registration not found.");
+            return "";
+        }
+        // Check if app was updated; if so, it must clear the registration ID
+        // since the existing regID is not guaranteed to work with the new
+        // app version.
+        int registeredVersion = prefs.getInt(PROPERTY_APP_VERSION, Integer.MIN_VALUE);
+        int currentVersion = getAppVersion(context);
+        if (registeredVersion != currentVersion) {
+            Log.i(TAG, "App version changed.");
+            return "";
+        }
+        return registrationId;
+    }
+
+    /**
+     * @return Application's {@code SharedPreferences}.
+     */
+    private SharedPreferences getGCMPreferences(Context context) {
+        // This sample app persists the registration ID in shared preferences, but
+        // how you store the regID in your app is up to you.
+        return getSharedPreferences(MainActivity.class.getSimpleName(),
+                Context.MODE_PRIVATE);
+    }
+
+    /**
+     * @return Application's version code from the {@code PackageManager}.
+     */
+    private static int getAppVersion(Context context) {
+        try {
+            PackageInfo packageInfo = context.getPackageManager()
+                    .getPackageInfo(context.getPackageName(), 0);
+            return packageInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            // should never happen
+            throw new RuntimeException("Could not get package name: " + e);
+        }
+    }
+
+    /**
+     * Registers the application with GCM servers asynchronously.
+     * <p>
+     * Stores the registration ID and app versionCode in the application's
+     * shared preferences.
+     */
+    private void registerInBackground() {
+        new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(Void... params) {
+                String msg = "";
+                try {
+                    if (gcm == null) {
+                        gcm = GoogleCloudMessaging.getInstance(context);
+                    }
+                    regid = gcm.register(SENDER_ID);
+                    msg = "Device registered, registration ID=" + regid;
+
+                    // You should send the registration ID to your server over HTTP,
+                    // so it can use GCM/HTTP or CCS to send messages to your app.
+                    // The request to your server should be authenticated if your app
+                    // is using accounts.
+                    //sendRegistrationIdToBackend();
+
+                    // For this demo: we don't need to send it because the device
+                    // will send upstream messages to a server that echo back the
+                    // message using the 'from' address in the message.
+
+                    // Persist the regID - no need to register again.
+                    storeRegistrationId(context, regid);
+                } catch (IOException ex) {
+                    msg = "Error :" + ex.getMessage();
+                    // If there is an error, don't just keep trying to register.
+                    // Require the user to click a button again, or perform
+                    // exponential back-off.
+                }
+                return msg;
+            }
+
+            protected void onPostExecute(String msg) {
+                Log.d("ONPostExecute",msg);
+            }
+        }.execute(null, null, null);
+
+    }
+
+    /**
+     * Stores the registration ID and app versionCode in the application's
+     * {@code SharedPreferences}.
+     *
+     * @param context application's context.
+     * @param regId registration ID
+     */
+    private void storeRegistrationId(Context context, String regId) {
+        final SharedPreferences prefs = getGCMPreferences(context);
+        int appVersion = getAppVersion(context);
+        Log.i(TAG, "Saving regId on app version " + appVersion);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(PROPERTY_REG_ID, regId);
+        editor.putInt(PROPERTY_APP_VERSION, appVersion);
+        editor.commit();
+    }
+
 }
---
+++ Mobile/RSC/app/src/main/java/com/alpha/asyncro/rsc/data/model/Notification.java
@@ -0,0 +1,9 @@
+package com.alpha.asyncro.rsc.data.model;
+
+/**
+ * Created by lovro on 22/11/14.
+ */
+public class Notification  {
+    //Received: Bundle[{from=957145599987, message=Hello World, i`m a push message, android.support.content.wakelockid=1, collapse_key=do_not_collapse}]
+
+}
