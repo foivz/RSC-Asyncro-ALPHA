80348e49f693860feb403e720bcc8d5e0c0f10f3
Lovro Predovan <lovro.predovan@gmail.com>
2014-11-23 03:42:06 +0100

    GCM moved to user registration fragment

--- Mobile/RSC/app/src/main/java/com/alpha/asyncro/rsc/MainActivity.java
+++ Mobile/RSC/app/src/main/java/com/alpha/asyncro/rsc/MainActivity.java
@@ -47,26 +47,6 @@ import retrofit.client.Response;
 public class MainActivity extends LightTabbedActivity implements OnDataMultipleResponseListener, OnErrorListener {
 
 
-    /**
-     * GCM
-     */
-    public static final String EXTRA_MESSAGE = "message";
-    public static final String PROPERTY_REG_ID = "registration_id";
-    private static final String PROPERTY_APP_VERSION = "appVersion";
-    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
-    String SENDER_ID = "957145599987";
-
-    /**
-     * Tag used on log messages.
-     */
-    static final String TAG = "GCMDemo";
-
-    GoogleCloudMessaging gcm;
-    AtomicInteger msgId = new AtomicInteger();
-    SharedPreferences prefs;
-    Context context;
-    String regid;
-
     @InjectView(R.id.pager)
     ViewPager pager;
     @InjectView(R.id.tabs)
@@ -98,24 +78,6 @@ public class MainActivity extends LightTabbedActivity implements OnDataMultipleR
             eventsFragment.setLabel(getString(R.string.mic_event));
             fragments = new Fragment[]{userFragment, donationsFragment, institutionsFragment, eventsFragment};
 
-            /**
-             * GCM
-             */
-
-            context = getApplicationContext();
-
-            // Check device for Play Services APK. If check succeeds, proceed with
-            //  GCM registration.
-            if (checkPlayServices()) {
-                gcm = GoogleCloudMessaging.getInstance(this);
-                regid = getRegistrationId(context);
-
-                if (regid.isEmpty()) {
-                    registerInBackground();
-                }
-            } else {
-                Log.i(TAG, "No valid Google Play Services APK found.");
-            }
             fragments = new Fragment[]{userFragment, donationsFragment, institutionsFragment, eventsFragment};
         }
         return fragments;
@@ -188,87 +150,11 @@ public class MainActivity extends LightTabbedActivity implements OnDataMultipleR
         return super.onOptionsItemSelected(item);
     }
 
-    private boolean checkPlayServices() {
-        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
-        if (resultCode != ConnectionResult.SUCCESS) {
-            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
-                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
-                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
-            } else {
-                Log.i(TAG, "This device is not supported.");
-                finish();
-            }
-            return false;
-        }
-        return true;
-    }
 
 
-    private String getRegistrationId(Context context) {
-        final SharedPreferences prefs = getGCMPreferences(context);
-        String registrationId = prefs.getString(PROPERTY_REG_ID, "");
-        if (registrationId.isEmpty()) {
-            Log.i(TAG, "Registration not found.");
-            return "";
-        }
-        int registeredVersion = prefs.getInt(PROPERTY_APP_VERSION, Integer.MIN_VALUE);
-        int currentVersion = getAppVersion(context);
-        if (registeredVersion != currentVersion) {
-            Log.i(TAG, "App version changed.");
-            return "";
-        }
-        return registrationId;
-    }
 
-    private SharedPreferences getGCMPreferences(Context context) {
-        return getSharedPreferences(MainActivity.class.getSimpleName(),
-                Context.MODE_PRIVATE);
-    }
 
-    private static int getAppVersion(Context context) {
-        try {
-            PackageInfo packageInfo = context.getPackageManager()
-                    .getPackageInfo(context.getPackageName(), 0);
-            return packageInfo.versionCode;
-        } catch (PackageManager.NameNotFoundException e) {
-            throw new RuntimeException("Could not get package name: " + e);
-        }
-    }
 
-    private void registerInBackground() {
-        new AsyncTask<Void, Void, String>() {
-            @Override
-            protected String doInBackground(Void... params) {
-                String msg = "";
-                try {
-                    if (gcm == null) {
-                        gcm = GoogleCloudMessaging.getInstance(context);
-                    }
-                    regid = gcm.register(SENDER_ID);
-                    msg = "Device registered, registration ID=" + regid;
-                    storeRegistrationId(context, regid);
 
-                } catch (IOException ex) {
-                    msg = "Error :" + ex.getMessage();
-                }
-                return msg;
-            }
-
-            protected void onPostExecute(String msg) {
-                Log.d("ONPostExecute", msg);
-            }
-        }.execute(null, null, null);
-
-    }
-
-    private void storeRegistrationId(Context context, String regId) {
-        final SharedPreferences prefs = getGCMPreferences(context);
-        int appVersion = getAppVersion(context);
-        Log.i(TAG, "Saving regId on app version " + appVersion);
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.putString(PROPERTY_REG_ID, regId);
-        editor.putInt(PROPERTY_APP_VERSION, appVersion);
-        editor.commit();
-    }
 
 }
--- Mobile/RSC/app/src/main/java/com/alpha/asyncro/rsc/fragment/RegisterFragment.java
+++ Mobile/RSC/app/src/main/java/com/alpha/asyncro/rsc/fragment/RegisterFragment.java
@@ -1,14 +1,27 @@
 package com.alpha.asyncro.rsc.fragment;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import com.alpha.asyncro.rsc.MainActivity;
 import com.alpha.asyncro.rsc.R;
 import com.alpha.asyncro.rsc.data.controller.UserController;
 import com.alpha.asyncro.rsc.data.model.User;
 import com.alpha.asyncro.rsc.event.OnRegisterListener;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
 import com.lightandroid.type.property.Labeled;
+
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import butterknife.InjectView;
 import butterknife.OnClick;
@@ -18,6 +31,29 @@ import butterknife.OnClick;
  */
 public class RegisterFragment extends LabeledFragment implements Labeled {
 
+    /**
+     * GCM
+     */
+    public static final String EXTRA_MESSAGE = "message";
+    public static final String PROPERTY_REG_ID = "registration_id";
+    private static final String PROPERTY_APP_VERSION = "appVersion";
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    String SENDER_ID = "957145599987";
+
+    /**
+     * Tag used on log messages.
+     */
+    static final String TAG = "GCMDemo";
+
+    GoogleCloudMessaging gcm;
+    AtomicInteger msgId = new AtomicInteger();
+    SharedPreferences prefs;
+    Context context;
+    String regid;
+
+    /**
+     * Injectings
+     */
     @InjectView(R.id.etEmail)
     EditText etEmail;
     @InjectView(R.id.etPassword)
@@ -53,11 +89,116 @@ public class RegisterFragment extends LabeledFragment implements Labeled {
         user.setPassword(etPassword.getText().toString());
         user.setName(etFirstName.getText().toString());
         user.setSurname(etLastName.getText().toString());
-        user.setRegId("ajdwiahahahadafguaapwoj");
+        /**
+         * GCM
+         */
+
+        context = getActivity();
+
+        // Check device for Play Services APK. If check succeeds, proceed with
+        //  GCM registration.
+        if (checkPlayServices()) {
+            gcm = GoogleCloudMessaging.getInstance(getActivity());
+            regid = getRegistrationId(context);
+
+            if (regid.isEmpty()) {
+                registerInBackground();
+            }
+        } else {
+            Log.i(TAG, "No valid Google Play Services APK found.");
+        }
+
+        /**
+         * Gcm end
+         */
+        user.setRegId(regid);
         user.setBloodGroup(2);
         onRegisterListener = new OnRegisterListener(getActivity(), user, userController);
         userController.setOnDataResponseListener(onRegisterListener);
         userController.register(user);
+    }
+
+    /**
+     * GCM methods
+     */
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getActivity());
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, getActivity(),
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Log.i(TAG, "This device is not supported.");
+            }
+            return false;
+        }
+        return true;
+    }
+
+    private String getRegistrationId(Context context) {
+        final SharedPreferences prefs = getGCMPreferences(context);
+        String registrationId = prefs.getString(PROPERTY_REG_ID, "");
+        if (registrationId.isEmpty()) {
+            Log.i(TAG, "Registration not found.");
+            return "";
+        }
+        int registeredVersion = prefs.getInt(PROPERTY_APP_VERSION, Integer.MIN_VALUE);
+        int currentVersion = getAppVersion(context);
+        if (registeredVersion != currentVersion) {
+            Log.i(TAG, "App version changed.");
+            return "";
+        }
+        return registrationId;
+    }
+
+    private void registerInBackground() {
+        new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(Void... params) {
+                String msg = "";
+                try {
+                    if (gcm == null) {
+                        gcm = GoogleCloudMessaging.getInstance(context);
+                    }
+                    regid = gcm.register(SENDER_ID);
+                    msg = "Device registered, registration ID=" + regid;
+                    storeRegistrationId(context, regid);
+
+                } catch (IOException ex) {
+                    msg = "Error :" + ex.getMessage();
+                }
+                return msg;
+            }
+
+            protected void onPostExecute(String msg) {
+                Log.d("ONPostExecute", msg);
+            }
+        }.execute(null, null, null);
+    }
+
+    private void storeRegistrationId(Context context, String regId) {
+        final SharedPreferences prefs = getGCMPreferences(context);
+        int appVersion = getAppVersion(context);
+        Log.i(TAG, "Saving regId on app version " + appVersion);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(PROPERTY_REG_ID, regId);
+        editor.putInt(PROPERTY_APP_VERSION, appVersion);
+        editor.commit();
+    }
+
+    private SharedPreferences getGCMPreferences(Context context) {
+        return getActivity().getSharedPreferences(LoginFragment.class.getSimpleName(),
+                Context.MODE_PRIVATE);
+    }
+
+    private static int getAppVersion(Context context) {
+        try {
+            PackageInfo packageInfo = context.getPackageManager()
+                    .getPackageInfo(context.getPackageName(), 0);
+            return packageInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            throw new RuntimeException("Could not get package name: " + e);
+        }
     }
 
 }
